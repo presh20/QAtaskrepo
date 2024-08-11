cbb08b2d5915e4fd9a5f54aa67aef22440387d30
presh20 <preciousonyinyechukwu21@gmail.com>
2024-08-11 14:37:36 +0100

    Trust crow commit

--- cypress/e2e/trustcrow.cy.js
+++ cypress/e2e/trustcrow.cy.js
@@ -4,7 +4,7 @@
      describe('Login Automation Test', () => {
      it('log in with valid credentials', () => {
      
-      const loginUrl = 'https://invoicing-backend-eozb.onrender.com/api/auth/login';
+      const loginUrl = 'https://invoicing-backend-eozb.onrender.com/api/auth/signup';
       const email = 'joe@mailinator.com'; 
       const password = 'Joe2024@';
     
@@ -18,11 +18,14 @@
         failOnStatusCode: false
       }).then ((response)=> {
       expect(response.status).to.eq(200);
-
      })
 
      })
      })
+
+     // RESPONSE RETURNED 400..FAILED TO RETURN 200
+     // THIS INDICATES A BAD REQUEST WAS PASSED
+     // Url is invalid for the payload been passed, should be a login url and not a signUp url.
 
 
 
